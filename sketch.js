250665e62833d8620642a94ff3e45df42fccd86d
henningpenning <henning.lundin@gmail.com>
2021-04-08 19:02:37 +0200

    add score 😀👦👦👦👦 menu to the game  and highscore

--- sketch.js
+++ sketch.js
@@ -4,8 +4,13 @@ var frameCount = 100;
 var screenWidth = document.documentElement.clientWidth;
 var score = 0;
 var screenHeight = document.documentElement.clientHeight;
+var mode;
+var highestScore = 0;
+
 function setup() {
+    mode = 0;
     createCanvas(screenWidth,screenHeight)
+
     bird = new Bird();
 
     pipes.push(new Pipe()); 
@@ -13,41 +18,66 @@ function setup() {
 
 function draw() {
    background(0);
-   showScore();
 
-   for (var i = pipes.length-1; i >= 0; i--) {    
-        pipes[i].show();
-        pipes[i].update();
-        if (pipes[i].hits(bird)) {
-            background(255, 0, 0);
-            score = 0;
-        }
-
-        if (pipes[i].offscreen()) {
-            pipes.splice(i, 1);
-
-        }  
+   if (mode==0) {   
+    fill(255);
+    textSize(100);
+    text(`press anything`,width/4,height/2);  
+    if(highestScore != 0) {
+        fill(255);
+        textSize(80);
+        text(`highscore: ${highestScore}`,width/4,(height/2) + 100);  
+        score = 0;
     }
+    
 
-   bird.update(); 
-   bird.show();
+   } else {
+        showScore();
 
-    if (frameCount % 100 == 0) {    
-        pipes.push(new Pipe());
-        score = score + 1;
-    }   
+        for (var i = pipes.length-1; i >= 0; i--) {    
+                pipes[i].show();
+                pipes[i].update();
+                if (pipes[i].hits(bird)) {
+                    background(255, 0, 0);
+                    mode=0; 
+                    pipes = [];
+                    
+                    if(highestScore < score) {
+                        highestScore = score;
+                    }
+                    score = 0;
+                    break;
+                }
+
+                if (pipes[i].offscreen()) {
+                    pipes.splice(i, 1);
+                }  
+            }
+
+        bird.update(); 
+        bird.show();
+
+            if (frameCount % 100 == 0) {    
+                pipes.push(new Pipe());
+                score = score + 1;
+            }   
+    }
 }
 
 function touchStarted() { 
     bird.up(); 
+    mode = 1;
 } 
 
 function mouseClicked() { 
     bird.up(); 
+    mode = 1;
 } 
 
 function keyPressed() {
     if(key == ' ') {
+        mode = 1;
+
         bird.up();
     }
 }
@@ -56,5 +86,4 @@ function showScore() {
     fill(255);
     textSize(100);
     text(score,100,100);
-    
 }
